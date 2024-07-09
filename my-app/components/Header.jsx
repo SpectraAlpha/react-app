604b56761ad6e8c9456d53dfea9b305235a3dc15
SpectraAlpha <119772400+SpectraAlpha@users.noreply.github.com>
2024-07-09 12:16:38 +0000

    init: styles

---
+++ my-app/components/Header.jsx
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+import { Button } from './ui/button'
+
+const Header=()=>{
+    return (
+        <header>
+            <h1>My App</h1>
+        </header>
+    )
+};
+
+export default Header;
--- my-app/tailwind.config.ts
+++ my-app/tailwind.config.ts
@@ -1,3 +1,4 @@
+import { X } from "lucide-react"
 import type { Config } from "tailwindcss"
 
 const config = {
@@ -12,51 +13,24 @@ const config = {
   theme: {
     container: {
       center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
+      padding: "15px",
+    },
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },
+    fontFamily: {
+      primary: "var(--font-jetbrainsMono)",
     },
     extend: {
       colors: {
-        border: "hsl(var(--border))",
-        input: "hsl(var(--input))",
-        ring: "hsl(var(--ring))",
-        background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
-        primary: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
-        },
-        secondary: {
-          DEFAULT: "hsl(var(--secondary))",
-          foreground: "hsl(var(--secondary-foreground))",
-        },
-        destructive: {
-          DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))",
-        },
-        muted: {
-          DEFAULT: "hsl(var(--muted))",
-          foreground: "hsl(var(--muted-foreground))",
-        },
-        accent: {
-          DEFAULT: "hsl(var(--accent))",
-          foreground: "hsl(var(--accent-foreground))",
-        },
-        popover: {
-          DEFAULT: "hsl(var(--popover))",
-          foreground: "hsl(var(--popover-foreground))",
-        },
-        card: {
-          DEFAULT: "hsl(var(--card))",
-          foreground: "hsl(var(--card-foreground))",
-        },
-      },
-      borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
+        primary: '#1c1c22',
+          accent:{
+            DEFAULT: '#00ff99',
+            hover: '#00e68a',
+          }
       },
       keyframes: {
         "accordion-down": {
